36464b288b5eef1b7db115d83f0a374212fe2b82
Micah Elijah <melijah200@gmail.com>
2020-07-27 16:42:44 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express"),
+            request = require("request"),
+            socket = require("socket.io"),
+            http = require("http");
+
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
+app.use(express.static("public"));
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket)=>{
+    socket.on("link", (data)=>{
+        const url = data;
+        request(url, (req, res, body)=>{
+            socket.emit("content", body);
+         });
+})
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, ()=>{
+    console.log(`Server running on PORT: ${PORT}`);
+});
